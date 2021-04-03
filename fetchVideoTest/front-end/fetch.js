e463b872e81595a2c9c4785cd313e7564b456550
Aidan McBride <aidankmcbride@gmail.com>
2021-04-02 23:00:16 -0400

    wip

---
+++ fetchVideoTest/front-end/fetch.js
@@ -0,0 +1,23 @@
+let getBtn = document.querySelector('.get-button');
+let postBtn = document.querySelector('.post-button');
+
+getBtn.addEventListener('click', () => {
+    console.log("GET")
+    fetch('http://localhost:3001/api')
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
+
+postBtn.addEventListener('click', () => {
+    let localVar =  { objName: "testing a post request" }
+    fetch('http://localhost:3001/add',
+    {
+        method: 'POST',
+        body: JSON.stringify(localVar),
+        // headers: {
+        //     'Content-Type' : 'application/json'
+        // }
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+})
