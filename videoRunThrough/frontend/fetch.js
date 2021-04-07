b84856c8ced454bc9eff162d76d90fa2638e3f89
Aidan McBride <aidankmcbride@gmail.com>
2021-04-06 23:16:29 -0400

    post and backup db

--- videoRunThrough/frontend/fetch.js
+++ videoRunThrough/frontend/fetch.js
@@ -1,26 +1,75 @@
-
+// GET
 let getBtn = document.querySelector('.get-btn')
-
+let flowersGrid = document.querySelector('.flowers-grid')
 
 const getFlowers = () => {
     fetch('http://localhost:3000/flowers')
     .then(response => response.json())
-    .then(flowers => console.log(flowers))
+    .then(flowers => {
+        flowers.forEach( flower => {
+            displayFlower(flower);
+        })
+    })
 }
 
-getBtn.addEventListener('click', getFlowers)
+getFlowers();
 
+// DELETE
+const deleteFlower = (flower) => {
+    fetch(`http://localhost:3000/flowers/${flower.id}`, {
+    method: 'DELETE',
+    headers: {
+        'Content-Type' : 'application/json'
+    }
+    })
+    .then(res => res.json())
+    .then(data => data)
+    flowerCard.remove()
+}
+
+const displayFlower = (flower) => {
+    let flowerCard = document.createElement('div')
+    flowerCard.className = 'flower-card'
+
+    let title = document.createElement('p')
+    title.className = 'flower-name'
+    title.innerText = flower.name
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.className = 'del-btn'
+    deleteBtn.innerText =  'X'
+
+    let img = document.createElement('img')
+    img.src = flower.imgUrl
+    
+    let sunExposure = document.createElement('p')
+    sunExposure.innerText = flower.sunExposure 
+
+    flowerCard.append(title, img, sunExposure, deleteBtn)
+    flowersGrid.append(flowerCard)
+
+    deleteBtn.addEventListener('click', () => {
+        fetch(`http://localhost:3000/flowers/${flower.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type' : 'application/json'
+            }
+            })
+            .then(res => res.json())
+            .then(data => console.log(data))
+            flowerCard.remove()
+    })
+}
+
+// POST
 let addFlowerForm = document.querySelector('.add-flower-form')
-// let addFlowerBtn = document.querySelector('add-flower-btn')
 
 const addFlower = (event) => {
     event.preventDefault()
     let newFlower = {
       name: event.target[0].value,
-      color: event.target[1].value,
-      type: event.target[2].value,
-      sun: event.target[3].value,
-      imgUrl: event.target[4].value,
+      sunExposure: event.target[1].value,
+      imgUrl: event.target[2].value,
     }
     fetch('http://localhost:3000/flowers', {
         method: 'POST',
@@ -34,7 +83,5 @@ const addFlower = (event) => {
 }
 
 addFlowerForm.addEventListener('submit', (event) => addFlower(event))
-//  (event) => {
-//     event.preventDefault()
-//     console.log(event.target)
-// })
+
+// PATCH
